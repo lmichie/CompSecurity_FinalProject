da60a525424bfef6340a0d9e43998fae7703b34d
Lindsey Michie <lmichie@nd.edu>
2022-04-08 12:55:25 -0400

    updates to rsa and gui

--- Cryptographic-Cipher-Tools/Cryptography/All_Crypto_GUI.py
+++ Cryptographic-Cipher-Tools/Cryptography/All_Crypto_GUI.py
@@ -5,7 +5,7 @@ import webbrowser
 from tkinter import ttk
 from time import strftime
 from PIL import Image, ImageTk
-from ciphers import caesarC, railfenceC, columnerC,vignereC
+from ciphers import rsaC, vignereC
 
 
 root = Tk()
@@ -22,254 +22,256 @@ icon.pack()
 
 ################################################################################################################################
 def cipher():
-    root.destroy()
+	root.destroy()
 
-    window = Tk()
+	window = Tk()
 
-    window.configure(background='gray')
-    window.title("Cryptographic ciphers")
+	window.configure(background='gray')
+	window.title("Cryptographic ciphers")
 
 
-    left_frame = Frame(window, width=200, height=600, relief=SUNKEN)
-    left_frame.pack(side=LEFT)
+	left_frame = Frame(window, width=200, height=600, relief=SUNKEN)
+	left_frame.pack(side=LEFT)
 
-    main_frame = Frame(window, width=800, height=100, relief=SUNKEN, borderwidth=10)
-    main_frame.pack()
+	main_frame = Frame(window, width=800, height=100, relief=SUNKEN, borderwidth=10)
+	main_frame.pack()
 
-    main = Frame(window, width=800, height=400, relief=SUNKEN, bg='#BF4340')
-    main.pack()
+	main = Frame(window, width=800, height=400, relief=SUNKEN, bg='#BF4340')
+	main.pack()
 
-    time_frame = Frame(window, width=500, height=30, relief=SUNKEN, background='gray')
-    time_frame.pack(side=BOTTOM)
+	time_frame = Frame(window, width=500, height=30, relief=SUNKEN, background='gray')
+	time_frame.pack(side=BOTTOM)
 
-    ###############################################################################################################################
+	###############################################################################################################################
 
-    def remove():
-        for widget in main.winfo_children():
-            widget.destroy()
+	def remove():
+		for widget in main.winfo_children():
+			widget.destroy()
 
-    def window_show():
-        remove()
+	def window_show():
+		remove()
 
-    def lab():
+	def lab():
 
-        text_label = Label(main, text="Enter text: ", font=('fixedsys', 16, "bold"), bg="tomato", fg="white")
-        text_label.grid(row=0, column=0, padx=20, pady=20)
+		text_label = Label(main, text="Enter text: ", font=('fixedsys', 16, "bold"), bg="tomato", fg="white")
+		text_label.grid(row=0, column=0, padx=20, pady=20)
 
-        scroll_text = ttk.Scrollbar(main, orient=VERTICAL)
-        text_box = Text(main, height=8, width=40, pady=10, yscrollcommand=scroll_text.set, bg='white')
-        text_box.grid(row=1, column=0, pady=1, padx=1)
-        scroll_text.config(command=text_box.yview)
-        scroll_text.grid(row=1, column=1, sticky='NS')
+		scroll_text = ttk.Scrollbar(main, orient=VERTICAL)
+		text_box = Text(main, height=8, width=40, pady=10, yscrollcommand=scroll_text.set, bg='white')
+		text_box.grid(row=1, column=0, pady=1, padx=1)
+		scroll_text.config(command=text_box.yview)
+		scroll_text.grid(row=1, column=1, sticky='NS')
 
-        key_label = Label(main, text="Enter key: ", font=('fixedsys', 14), pady=15,
-                          bg='black', fg="cyan")
-        key_label.grid(row=2, column=0)
+		key_label = Label(main, text="Enter key: ", font=('fixedsys', 14), pady=15,
+						  bg='black', fg="cyan")
+		key_label.grid(row=2, column=0)
 
-        scroll_text2 = ttk.Scrollbar(main, orient=VERTICAL)
-        new_text = Text(main, height=8, width=40, pady=10, yscrollcommand=scroll_text2.set, bg='white')
-        new_text.grid(row=1, column=2, columnspan=2, padx=(10, 0))
-        scroll_text2.config(command=new_text.yview)
-        scroll_text2.grid(row=1, column=4, sticky='NS')
-        return text_box, new_text
+		scroll_text2 = ttk.Scrollbar(main, orient=VERTICAL)
+		new_text = Text(main, height=8, width=40, pady=10, yscrollcommand=scroll_text2.set, bg='white')
+		new_text.grid(row=1, column=2, columnspan=2, padx=(10, 0))
+		scroll_text2.config(command=new_text.yview)
+		scroll_text2.grid(row=1, column=4, sticky='NS')
+		return text_box, new_text
 
-    ################################################################################################################################
-    def RSA_cipher():  # 1st cipher
+	################################################################################################################################
+	def RSA_cipher():  # 1st cipher
 
-        remove()
+		remove()
 
 
-        list_key = ttk.Combobox(main)
-        list_key['values'] = (
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
-        list_key.current(0)
-        list_key.grid(row=5, column=0)
+		list_key = ttk.Combobox(main)
+		list_key['values'] = (
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
+		list_key.current(0)
+		list_key.grid(row=5, column=0)
+
+		text_box, new_text = lab()
+		sample = ""
+		new_text.insert(1.0, sample)
+		label = Label(main, text="RSA Encryption" , font=('Times New Roman', 16, "bold"),bg="#FDA73F")
+		label.grid(row=0, column=1)
+		#Generatate the keys
+		e, d, n = rsaC.gen_values()
 
-        text_box, new_text = lab()
-        sample = ""
-        new_text.insert(1.0, sample)
-        label = Label(main, text="RSA Encryption" , font=('Times New Roman', 16, "bold"),bg="#FDA73F")
-        label.grid(row=0, column=1)
+		def encrypt():
+			new_text.delete('1.0', END)
+			txt = text_box.get("1.0", END)
+			key = int(list_key.get())
+			enc_text = rsaC.encryption(txt, e, n)
+			new_text.insert(1.0, enc_text)
 
-        def encrypt():
-            new_text.delete('1.0', END)
-            txt = text_box.get("1.0", END)
-            key = int(list_key.get())
-            enc_text = caesarC.encryption(txt, key)
-            new_text.insert(1.0, enc_text)
+		enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,bg='#3FBE7F', fg='white')
+		enc.grid(row=0, column=2, padx=20, pady=30)
+		def decrypt():
+			new_text.delete('1.0', END)
+			txt = text_box.get("1.0", END)
+			key = int(list_key.get())
+			dec_text = caesarC.decryption(txt, key)
+			new_text.insert(1.0, dec_text)
 
-        enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,bg='#3FBE7F', fg='white')
-        enc.grid(row=0, column=2, padx=20, pady=30)
-        def decrypt():
-            new_text.delete('1.0', END)
-            txt = text_box.get("1.0", END)
-            key = int(list_key.get())
-            dec_text = caesarC.decryption(txt, key)
-            new_text.insert(1.0, dec_text)
+		dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,bg='tomato2', fg='white')
+		dec.grid(row=0, column=3, padx=10, pady=10)
+
+	#############################################################################################################################
 
-        dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,bg='tomato2', fg='white')
-        dec.grid(row=0, column=3, padx=10, pady=10)
 
-    #############################################################################################################################
+	##############################################################################################################################
+	def vignere_cipher():
+		remove()
 
+		key_text = Entry(main, width=40)
+		key_text.grid(row=3, column=0, padx=10, pady=10)
+
+		text_box, new_text = lab()
+		sample = " "
+		new_text.insert(1.0, sample)
+		label = Label(main, text="vignere_cipher")
+		label.grid(row=0, column=1)
+
+		def encrypt():
+			new_text.delete('1.0', END)
+			txt = text_box.get("1.0", END)
+			key = key_text.get()
+			enc_text = vignereC.encrypt(txt, key)
+			new_text.insert(1.0, enc_text)
+
+		enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,
+					 bg='#3FBE7F', fg='white')
+		enc.grid(row=0, column=2, padx=20, pady=30)
+
+		def decrypt():
+			new_text.delete('1.0', END)
+			txt = text_box.get("1.0", END)
+			key = key_text.get()
+			dec_text = vignereC.decrypt(txt, key)
+			new_text.insert(1.0, dec_text)
+
+		dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,
+					 bg='tomato2', fg='white')
+		dec.grid(row=0, column=3, padx=10, pady=10)
+
+	#############################################################################################################################
+
+	################################################################################################################################
+
+	def DES3_cipher():
+		remove()
 
-    ##############################################################################################################################
-    def vignere_cipher():
-        remove()
+		list_key = ttk.Combobox(main)
+		list_key['values'] = (2, 3, 4, 5, 6, 7, 8)
+		list_key.current(0)
+		list_key.grid(row=5, column=0)
 
-        key_text = Entry(main, width=40)
-        key_text.grid(row=3, column=0, padx=10, pady=10)
-
-        text_box, new_text = lab()
-        sample = " "
-        new_text.insert(1.0, sample)
-        label = Label(main, text="vignere_cipher")
-        label.grid(row=0, column=1)
-
-        def encrypt():
-            new_text.delete('1.0', END)
-            txt = text_box.get("1.0", END)
-            key = key_text.get()
-            enc_text = vignereC.encrypt(txt, key)
-            new_text.insert(1.0, enc_text)
-
-        enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,
-                     bg='#3FBE7F', fg='white')
-        enc.grid(row=0, column=2, padx=20, pady=30)
-
-        def decrypt():
-            new_text.delete('1.0', END)
-            txt = text_box.get("1.0", END)
-            key = key_text.get()
-            dec_text = vignereC.decrypt(txt, key)
-            new_text.insert(1.0, dec_text)
-
-        dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,
-                     bg='tomato2', fg='white')
-        dec.grid(row=0, column=3, padx=10, pady=10)
-
-    #############################################################################################################################
-
-    ################################################################################################################################
-
-    def DES3_cipher():
-        remove()
+		text_box, new_text = lab()
+		sample = " "
+		new_text.insert(1.0, sample)
+		label = Label(main, text="Triple DES")
+		label.grid(row=0, column=1)
 
-        list_key = ttk.Combobox(main)
-        list_key['values'] = (2, 3, 4, 5, 6, 7, 8)
-        list_key.current(0)
-        list_key.grid(row=5, column=0)
+		def encrypt():
+			new_text.delete('1.0', END)
+			string = (text_box.get("1.0", END)).strip()
+			key = int(list_key.get())
+			enc_text = railfenceC.encrypt(string, key)
+			new_text.insert(1.0, enc_text)
 
-        text_box, new_text = lab()
-        sample = " "
-        new_text.insert(1.0, sample)
-        label = Label(main, text="Triple DES")
-        label.grid(row=0, column=1)
+		enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,bg='#3FBE7F', fg='white')
+		enc.grid(row=0, column=2, padx=20, pady=30)
 
-        def encrypt():
-            new_text.delete('1.0', END)
-            string = (text_box.get("1.0", END)).strip()
-            key = int(list_key.get())
-            enc_text = railfenceC.encrypt(string, key)
-            new_text.insert(1.0, enc_text)
+		def decrypt():
+			new_text.delete('1.0', END)
+			string = (text_box.get("1.0", END)).strip()
+			key = int(list_key.get())
+			dec_text = railfenceC.decrypt(string, key)
+			new_text.insert(1.0, dec_text)
 
-        enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,bg='#3FBE7F', fg='white')
-        enc.grid(row=0, column=2, padx=20, pady=30)
+		dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,
+					 bg='tomato2', fg='white')
+		dec.grid(row=0, column=3, padx=10, pady=10)
 
-        def decrypt():
-            new_text.delete('1.0', END)
-            string = (text_box.get("1.0", END)).strip()
-            key = int(list_key.get())
-            dec_text = railfenceC.decrypt(string, key)
-            new_text.insert(1.0, dec_text)
+	###############################################################################################################################
 
-        dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,
-                     bg='tomato2', fg='white')
-        dec.grid(row=0, column=3, padx=10, pady=10)
 
-    ###############################################################################################################################
+	############################################################################################################################
+	def AES_cipher():
+		remove()
 
+		key_text = Entry(main, width=40)
+		key_text.grid(row=3, column=0, padx=10, pady=10)
 
-    ############################################################################################################################
-    def AES_cipher():
-        remove()
+		text_box, new_text = lab()
+		sample = " "
+		new_text.insert(1.0, sample)
+		label = Label(main, text="AES cipher")
+		label.grid(row=0, column=1)
 
-        key_text = Entry(main, width=40)
-        key_text.grid(row=3, column=0, padx=10, pady=10)
+		def encrypt():
+			new_text.delete('1.0', END)
+			txt = text_box.get("1.0", END)
+			key = key_text.get().lower()
+			enc_text = columnerC.encrypt(txt, key)
+			new_text.insert(1.0, enc_text)
 
-        text_box, new_text = lab()
-        sample = " "
-        new_text.insert(1.0, sample)
-        label = Label(main, text="AES cipher")
-        label.grid(row=0, column=1)
+		enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,bg='#3FBE7F', fg='white')
+		enc.grid(row=0, column=2, padx=20, pady=30)
 
-        def encrypt():
-            new_text.delete('1.0', END)
-            txt = text_box.get("1.0", END)
-            key = key_text.get().lower()
-            enc_text = columnerC.encrypt(txt, key)
-            new_text.insert(1.0, enc_text)
+		def decrypt():
+			new_text.delete('1.0', END)
+			txt = text_box.get("1.0", END)
+			key = key_text.get().lower()
+			dec_text = columnerC.decrypt(txt, key)
+			new_text.insert(1.0, dec_text)
 
-        enc = Button(main, text="Encrypt", bd=10, width=10, command=encrypt,bg='#3FBE7F', fg='white')
-        enc.grid(row=0, column=2, padx=20, pady=30)
+		dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,
+					 bg='tomato2', fg='white')
+		dec.grid(row=0, column=3, padx=10, pady=10)
 
-        def decrypt():
-            new_text.delete('1.0', END)
-            txt = text_box.get("1.0", END)
-            key = key_text.get().lower()
-            dec_text = columnerC.decrypt(txt, key)
-            new_text.insert(1.0, dec_text)
+	###########################################################################################################################
+	# buttons on window left lide
+	home_button = Button(left_frame, padx=20, bd=10, text='Home', width=20, height=3, command=window_show,
+						 bg='#FDA73F', fg='white', activebackground='black', font=('arial', 16, 'bold'),
+						 activeforeground='#3FBE4F')
+	home_button.grid(row=0, column=0)
 
-        dec = Button(main, text="Decrypt", bd=10, width=10, command=decrypt,
-                     bg='tomato2', fg='white')
-        dec.grid(row=0, column=3, padx=10, pady=10)
+	RSA = Button(left_frame, padx=20, bd=10, text='RSA Cipher', width=20, height=3, command=RSA_cipher,
+					bg='white', fg='red', activebackground='black', font=('arial', 16, 'bold'),
+					activeforeground='#3FBE4F')
+	RSA.grid(row=1, column=0)
 
-    ###########################################################################################################################
-    # buttons on window left lide
-    home_button = Button(left_frame, padx=20, bd=10, text='Home', width=20, height=3, command=window_show,
-                         bg='#FDA73F', fg='white', activebackground='black', font=('arial', 16, 'bold'),
-                         activeforeground='#3FBE4F')
-    home_button.grid(row=0, column=0)
 
-    RSA = Button(left_frame, padx=20, bd=10, text='RSA Cipher', width=20, height=3, command=RSA_cipher,
-                    bg='white', fg='red', activebackground='black', font=('arial', 16, 'bold'),
-                    activeforeground='#3FBE4F')
-    RSA.grid(row=1, column=0)
+	vignere = Button(left_frame, padx=20, bd=10, text='Vignere Cipher', width=20, height=3, command=vignere_cipher,
+					 bg='white', fg='red', activebackground='black', font=('arial', 16, 'bold'),
+					 activeforeground='#3FBE4F')
+	vignere.grid(row=2, column=0)
 
 
-    vignere = Button(left_frame, padx=20, bd=10, text='Vignere Cipher', width=20, height=3, command=vignere_cipher,
-                     bg='white', fg='red', activebackground='black', font=('arial', 16, 'bold'),
-                     activeforeground='#3FBE4F')
-    vignere.grid(row=2, column=0)
 
+	DES3 = Button(left_frame, padx=20, bd=10, text='Triple DES Cipher', width=20, height=3,
+					   command=DES3_cipher, bg='white', fg='red', activebackground='black',
+					   font=('arial', 16, 'bold'), activeforeground='#3FBE4F')
+	DES3.grid(row=3, column=0)
 
 
-    DES3 = Button(left_frame, padx=20, bd=10, text='Triple DES Cipher', width=20, height=3,
-                       command=DES3_cipher, bg='white', fg='red', activebackground='black',
-                       font=('arial', 16, 'bold'), activeforeground='#3FBE4F')
-    DES3.grid(row=3, column=0)
 
 
 
+	AES = Button(left_frame, padx=20, bd=10, text='AES Cipher', width=26, height=3,
+					  command=AES_cipher, bg='white', fg='red', font=('arial', 12, 'bold'),
+					  activebackground='black', activeforeground='SeaGreen1')
+	AES.grid(row=4, column=0)
 
 
-    AES = Button(left_frame, padx=20, bd=10, text='AES Cipher', width=26, height=3,
-                      command=AES_cipher, bg='white', fg='red', font=('arial', 12, 'bold'),
-                      activebackground='black', activeforeground='SeaGreen1')
-    AES.grid(row=4, column=0)
+	Exit = Button(left_frame, text="Exit", padx=20, bd=10, width=23,height=3, command=window.destroy, activebackground="red",
+				  bg='#8E7360', fg="white", font=('arial', 12, 'bold'), activeforeground="green2")
+	Exit.grid(row=15, column=0)
 
-
-    Exit = Button(left_frame, text="Exit", padx=20, bd=10, width=23,height=3, command=window.destroy, activebackground="red",
-                  bg='#8E7360', fg="white", font=('arial', 12, 'bold'), activeforeground="green2")
-    Exit.grid(row=15, column=0)
-
-    window.mainloop()
+	window.mainloop()
 
 
 Welcome_label = Label(root, text="Welcome to Cryptography ciphers", padx=10, pady=30, bg="light gray", fg="black",
-                      font=("Helvetica", 20, "bold")).pack(pady=30)
+					  font=("Helvetica", 20, "bold")).pack(pady=30)
 chiper_button = Button(root, text="Cryprocypher", padx=20, bd=10, width=20, height=3, command=cipher, bg="lightgreen",
-                       fg="darkblue", activebackground='black', activeforeground='SeaGreen1')
+					   fg="darkblue", activebackground='black', activeforeground='SeaGreen1')
 chiper_button.pack(pady=30)
 
 
--- Cryptographic-Cipher-Tools/Cryptography/ciphers/rsaC.py
+++ Cryptographic-Cipher-Tools/Cryptography/ciphers/rsaC.py
@@ -3,19 +3,58 @@ from math import gcd
 p = 448500690975152607978063537675655100228561968640743033264994624907311301727954618776592131432970453319274164757456900360809613450208308427051224715601825715694126659023243326653708932560006426538354434994211909696350939307259469268464399327307567418024237505252087309516604048747469861472120690296679
 q = 797142830676371202783106590305499953807978464831617412146739794698696574269657813361892048534053769911087232366556200569808067035904716519320282044787921526677805285799163501701004063525600697748650815091626560388355572839685532622148490117538712600176513505164969571526664640345723199170240816113027
 
-n = p * q
-phi_n = (p-1) * (q-1)
+def gen_values():
+	n = p * q
+	phi = (p-1) * (q-1)
 
-e = int(phi_n) - 1
-d = pow(e, -1, int(phi_n))
+	e = random.randrange(1,phi)
+	g = euclid(e,phi)
+	while(g!=1):
+		e = random.randrange(1,phi)
+		g = euclid(e,phi)
 
-def coprime(a, b):
-	return gcd(a,b) == 1
+	d = extended_euclid(e,phi)
+	public_key=(e,n)
+	private_key=(d,n)
+	return e, d, n
 
-def encrypt(plainText):
+def euclid(a, b):
+	if b==0:
+		return a
+	else:
+		return euclid(b, a % b)
+
+def extended_euclid(e,phi):
+	d=0
+	x1=0
+	x2=1
+	y1=1
+	orig_phi = phi
+	tempPhi = phi
+
+	while (e>0):
+		temp1 = int(tempPhi/e)
+		temp2 = tempPhi - temp1 * e
+		tempPhi = e
+		e = temp2
+
+		x = x2- temp1* x1
+		y = d - temp1 * y1
+
+		x2 = x1
+		x1 = x
+		d = y1
+		y1 = y
+
+		if tempPhi == 1:
+			d += phi
+			break
+	return d
+
+def encrypt(plainTexti, e, n):
 	plainText = plainText.strip()
 	b = bytes(plainText, 'utf-8')
-	cipherText = pow(int.from_bytes(b, byteorder='big', signed=False), int(e), int(n))
+	cipherText = (int.from_bytes(b, byteorder='big', signed=False)**e) % n
 	return str(cipherText)
 
 def decrypt(cipherText):
