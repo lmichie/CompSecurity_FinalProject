d9fa83b261a77c39916c9523912ed45d968f2556
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2022-05-01 16:38:35 -0400

    not sure what I changed to metrics, but pushing it

--- Cryptographic-Cipher-Tools/Cryptography/ciphers/metrics.py
+++ Cryptographic-Cipher-Tools/Cryptography/ciphers/metrics.py
@@ -7,49 +7,47 @@ import rsaC, vignereC, aesC, tripleC
 lengths = [10, 100, 1000, 10000]
 duration = defaultdict(list)
 
-# for length in lengths:
-#     n = 10
-#     text = 'a' * length
+for length in lengths:
+    n = 10
+    text = 'a' * length
     
-#     total = 0
-#     key = 'ndsecure'
-#     for _i in range(n):
-#         start = time()
-#         assert text == vignereC.decrypt(vignereC.encrypt(text, key), key)
-#         total += time() - start
-#     duration['vigenere'].append(total/n)
+    total = 0
+    key = 'ndsecure'
+    for _i in range(n):
+        start = time()
+        assert text == vignereC.decrypt(vignereC.encrypt(text, key), key)
+        total += time() - start
+    duration['vigenere'].append(total/n)
     
-#     total = 0
-#     e, p, q = 11, 113, 151
-#     for _i in range(n):
-#         start = time()
-#         assert text == rsaC.decryption(rsaC.encryption(text, e, p, q)[1], e, p, q)
-#         total += time() - start
-#     duration['rsa'].append(total/n)
+    total = 0
+    e, p, q = 11, 113, 151
+    for _i in range(n):
+        start = time()
+        assert text == rsaC.decryption(rsaC.encryption(text, e, p, q)[1], e, p, q)
+        total += time() - start
+    duration['rsa'].append(total/n)
     
-#     total = 0
-#     key0 = "0000000011111111000000001111111100000000111111110000000011111111"
-#     key1 = "0000000000000000000000000000000000000000000000000000000000000000"
-#     key2 = "1111111111111111111111111111111111111111111111111111111111111111"
-#     for _i in range(n):
-#         start = time()
-#         r = tripleC.triple_DES_encryption(text, key0, key1, key2)
-#         r = tripleC.triple_DES_decryption(r, key0, key1, key2)
-#         r = tripleC.binary_to_plaintext(r)
-#         r = r.rstrip('\0')
-#         assert text == r
-#         total += time() - start
-#     duration['3des'].append(total/n)
+    total = 0
+    key0 = "0000000011111111000000001111111100000000111111110000000011111111"
+    key1 = "0000000000000000000000000000000000000000000000000000000000000000"
+    key2 = "1111111111111111111111111111111111111111111111111111111111111111"
+    for _i in range(n):
+        start = time()
+        r = tripleC.triple_DES_encryption(text, key0, key1, key2)
+        r = tripleC.triple_DES_decryption(r, key0, key1, key2)
+        r = tripleC.binary_to_plaintext(r)
+        r = r.rstrip('\0')
+        assert text == r
+        total += time() - start
+    duration['3des'].append(total/n)
     
-#     total = 0
-#     key = list(bytes('ndsecurendsecure', encoding='utf-8'))
-#     for _i in range(n):
-#         start = time()
-#         assert text == aesC.decrypt_text(aesC.encrypt_text(text, key), key)
-#         total += time() - start
-#     duration['aes'].append(total/n)
-
-duration = {'vigenere': [0.0, 9.67264175415039e-05, 0.0015442848205566406, 0.009467864036560058], 'rsa': [0.0, 0.00011970996856689454, 0.003766322135925293, 0.03181643486022949], '3des': [0.014396142959594727, 0.06591246128082276, 0.5105249404907226, 5.759778952598571], 'aes': [0.0006993532180786133, 0.0025276422500610353, 0.019481778144836426, 0.4879094123840332]}
+    total = 0
+    key = list(bytes('ndsecurendsecure', encoding='utf-8'))
+    for _i in range(n):
+        start = time()
+        assert text == aesC.decrypt_text(aesC.encrypt_text(text, key), key)
+        total += time() - start
+    duration['aes'].append(total/n)
     
 # set width of bar
 bar_width = 0.1
